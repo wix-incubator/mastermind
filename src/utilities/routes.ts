b086cbea3a0d9699ccab10e9be26ac5079d56878
Eyal Eizenberg <eyale@wix.com>
2018-04-26 09:30:42 +0300

    new routes

--- src/utilities/routes.ts
+++ src/utilities/routes.ts
@@ -1,4 +1,5 @@
-export const ROOT_PATH = '/';
-export const GAME_PATH = '/games/:id';
-export const getRootPath = () => '/';
-export const getGamePath = (id: string) => `/games/${id}`;
+const PUBLIC_URL = process.env.PUBLIC_URL;
+export const ROOT_PATH = PUBLIC_URL + '/';
+export const GAME_PATH = PUBLIC_URL + '/games/:id';
+export const getRootPath = () => PUBLIC_URL + '/';
+export const getGamePath = (id: string) => PUBLIC_URL + `/games/${id}`;
