e53e3eac49ab3fa5165c87e2924e8b79470772c5
Eyal Eizenberg <eyale@wix.com>
2018-04-26 09:45:53 +0300

    first deploy!

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import SearchContainer from '../Search/Search.container';
+import { getRootPath } from '../../utilities/routes';
 const styles = require('./Header.scss');
 
 export default class Header extends React.PureComponent {
@@ -8,7 +9,7 @@ export default class Header extends React.PureComponent {
     return (
       <div className={styles.outerHeaderContainer}>
         <div className={styles.innerHeaderContainer}>
-          <Link to="/" className={styles.header}>
+          <Link to={getRootPath()} className={styles.header}>
             MASTERMIND
           </Link>
           <SearchContainer />
