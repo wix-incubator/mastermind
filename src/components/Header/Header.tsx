9748d45f53ee0697bbf5b1f96ab930bb93f13f72
Eyal Eizenberg <eyale@wix.com>
2018-04-27 09:54:57 +0300

    logo and header fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import SearchContainer from '../Search/Search.container';
 import { getRootPath } from '../../utilities/routes';
 const styles = require('./Header.scss');
+const logo = require('../../assets/images/logo.svg');
 
 export default class Header extends React.PureComponent {
   render() {
@@ -10,7 +11,8 @@ export default class Header extends React.PureComponent {
       <div className={styles.outerHeaderContainer}>
         <div className={styles.innerHeaderContainer}>
           <Link to={getRootPath()} className={styles.header}>
-            MASTERMIND
+            <img src={logo} className={styles.logo} />
+            <span>Mastermind</span>
           </Link>
           <SearchContainer />
         </div>
