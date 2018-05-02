b8d2d7bb1ff3e3da6412b92f73254dd9a1bf3e73
Eyal Eizenberg <eyale@wix.com>
2018-05-02 13:34:50 +0300

    load the game in an iframe

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -5,29 +5,85 @@ import GameHeaderContainer from '../GameHeader/GameHeader.container';
 import Spinner from '../Spinner/Spinner';
 import Button from '../Button/Button';
 import { scrollToGameDetails } from '../../utilities/scrollToElement';
+import * as classnames from 'classnames';
 const styles = require('./Game.scss');
 
-export default class Game extends React.PureComponent<IGame> {
+interface IGameState {
+  loadingGame: boolean;
+  gameLoaded: boolean;
+}
+
+export default class Game extends React.PureComponent<IGame, IGameState> {
+  constructor(props: IGame) {
+    super(props);
+    this.state = {
+      loadingGame: false,
+      gameLoaded: false
+    };
+  }
+
+  startGame = () => {
+    window.addEventListener('message', this.gameLoaded);
+    this.setState({ loadingGame: true });
+  };
+
+  gameLoaded = (event: any) => {
+    const { url } = this.props;
+
+    if (event.origin === url && event.message === 'gameLoaded') {
+      window.removeEventListener('message', this.gameLoaded);
+      this.setState({
+        loadingGame: false,
+        gameLoaded: true
+      });
+    }
+  };
+
+  renderGameImageOrSpinner() {
+    const { loadingGame, gameLoaded } = this.state;
+    const { imageUrl, url } = this.props;
+
+    if (loadingGame || gameLoaded) {
+      return (
+        <React.Fragment>
+          {loadingGame && <Spinner />}
+          <iframe
+            className={classnames(styles.gameFrame, {
+              [styles.gameFrameLoaded]: gameLoaded
+            })}
+            src={url}
+          />
+        </React.Fragment>
+      );
+    }
+
+    return (
+      <React.Fragment>
+        <img className={styles.game} src={imageUrl} />
+        <div className={styles.gameControls}>
+          <div className={styles.buttonsContainer}>
+            <Button onClick={this.startGame}>Play</Button>
+            <Button
+              className={styles.aboutGameButton}
+              onClick={scrollToGameDetails}
+            >
+              <React.Fragment>
+                <span>About Game</span>
+                <div className={styles.triangle} />
+              </React.Fragment>
+            </Button>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+
   renderContent() {
     return (
       <React.Fragment>
         <div className={styles.gameContainer}>
           <div className={styles.gameWrapper}>
-            <img className={styles.game} src={this.props.imageUrl} />
-            <div className={styles.gameControls}>
-              <div className={styles.buttonsContainer}>
-                <Button>Play</Button>
-                <Button
-                  className={styles.aboutGameButton}
-                  onClick={scrollToGameDetails}
-                >
-                  <React.Fragment>
-                    <span>About Game</span>
-                    <div className={styles.triangle} />
-                  </React.Fragment>
-                </Button>
-              </div>
-            </div>
+            {this.renderGameImageOrSpinner()}
           </div>
         </div>
         <GameHeaderContainer game={this.props} />
